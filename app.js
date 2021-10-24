fa9aa37b3d4a3768f0addfc4f1b155f32bf3485c
Nafi <work.nafi@gmail.com>
2021-10-24 08:03:10 +0600

    modified app.js file

--- app.js
+++ app.js
@@ -3,27 +3,35 @@ const http = require('http');
 
 const server = http.createServer(function(req, res) {
 
-            if (req.url == "/") {
-                let data = fs.readFileSync('index.html', 'utf8');
-                res.end(data);
+    if (req.url == "index.html") {
+        fs.readFile('index.html', function(err, data) {
+            // fs.writeFile(200, { 'content': 'text/html' })
+            // fs.write(data);
+            res.end(data);
+        })
 
-            } else if (req.url == "/Skills") {
-                let data = fs.readFileSync('skills.html', 'utf8');
-                res.end(data);
+    } else if (req.url == "skills.html") {
+        fs.readFile('skils.html', function(data) {
+            res.end(data);
+        })
+    } else if (req.url == "recent.html") {
+        fs.readFile('recent.html', function(data) {
+            res.end(data);
+        })
 
-            } else if (req.url == "/Recent") {
-                let data = fs.readFileSync('recent.html', 'utf8');
-                res.end(data);
+    } else if (req.url == "client.html") {
+        fs.readFile('client.html', function(data) {
+            res.end(data);
+        })
 
-            } else if (req.url == "/Client") {
-                let data = fs.readFileSync('client.html', 'utf8');
-                res.end(data);
+    } else {
+        fs.readFile('contact.html', function(data) {
+            res.end(data);
+        })
 
-            } else if ((req.url == "/Contact") {
-                    let data = fs.readFileSync('contact.html', 'utf8');
-                    res.end(data);
-                }
+    }
 
-            });
+});
 
-        server.listen(5000); console.log("server run success");
+server.listen(5000);
+console.log("server run success");
